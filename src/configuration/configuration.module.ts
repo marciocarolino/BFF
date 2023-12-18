382b3b39e769d7ac76b5dda08069046be38d049b
Marcio Carolino <marciocarolino@gmail.com>
2023-12-18 18:41:09 -0300

    feat: reading example json file

--- src/configuration/configuration.module.ts
+++ src/configuration/configuration.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigurationController } from './configuration.controller';
 import { ConfigurationService } from './configuration.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [],
+  imports: [HttpModule],
   controllers: [ConfigurationController],
   providers: [ConfigurationService],
   exports: [ConfigurationService],
--- src/configuration/configuration.service.ts
+++ src/configuration/configuration.service.ts
@@ -1,10 +1,29 @@
 import { Injectable } from '@nestjs/common';
-import { CreateConfigurationDTO } from './dto/configuration.dto';
+import axios from 'axios';
+import * as path from 'path';
+import * as fs from 'fs/promises';
 
 @Injectable()
 export class ConfigurationService {
   async getAll(): Promise<any> {
-    return 'GET ALL';
+    const filePath = path.join(__dirname, '../../', 'configuration.json');
+
+    try {
+      //Lê o conteúdo do arquivo de froma assíncrona
+      const fileContent = await fs.readFile(filePath, 'utf-8');
+
+      //faz a solicitação HTTP usando Axios para simular uma requisição
+      const response = await axios.get(
+        `data:application/json;base64,${Buffer.from(fileContent).toString(
+          'base64',
+        )}`,
+      );
+
+      return JSON.parse(response.data.toString());
+    } catch (error) {
+      console.log(`Error reading JSON file: ${error.message}`);
+      throw error;
+    }
   }
 
   async getById(id: number): Promise<any> {
