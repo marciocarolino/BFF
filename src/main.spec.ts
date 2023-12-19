bf13738797ddb7377aa5db679f0dde9573e96f78
Marcio Carolino <marciocarolino@gmail.com>
2023-12-19 09:56:50 -0300

    feat: basic tests completed

--- src/main.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AppService } from './app.service';
-
-describe('Main', () => {
-  it('should bootstrap successfully', async () => {
-    const appService = new AppService();
-    await expect(appService.bootstrap()).resolves.not.toThrow();
-  });
-});
